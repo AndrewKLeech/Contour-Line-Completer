d8fce5d9d4b79562e9fcc528f9991457c311b77d
AndrewKLeech <andrewkennethleech@gmail.com>
2017-10-17 20:19:55 +0100

    Add very unaccurate and messy lines to make Voronoi diagram

--- src/DisplayConnection.java
+++ src/DisplayConnection.java
@@ -13,7 +13,8 @@ public class DisplayConnection extends JFrame {
     static BufferedImage img;
     static int posX[], posY[];
     static int windowSize = 500;
-    static int numberOfPoints = 5;
+    static int numberOfPoints = 50;
+    static int n;
     public DisplayConnection() {
         super("Contour Connector");
         setBounds(0, 0, windowSize, windowSize);
@@ -32,9 +33,42 @@ public class DisplayConnection extends JFrame {
             posX[i] = rand.nextInt(windowSize);
             posY[i] = rand.nextInt(windowSize);
         }
+
+        for (int curX = 0; curX < windowSize; curX++) {//curr x
+            for (int curY = 0; curY < windowSize; curY++) {//curr y
+                for (int pointA = 0; pointA < numberOfPoints; pointA++) {//point
+                    double distA = euclidian(posX[pointA], curX, posY[pointA], curY);
+                    double distB = 0;
+                    double smallestDist = windowSize;
+                    for(int pointB = 0; pointB < numberOfPoints; pointB++){
+                        distB = euclidian(posX[pointB], curX, posY[pointB], curY);
+                        if(smallestDist>distB){
+                            smallestDist = distB;
+                        }
+                    }
+                    double margin = 0.5;
+                    if ((distA > smallestDist) && (distA<smallestDist+margin) || (distA < smallestDist) && (distA>smallestDist-margin) ||(smallestDist>distA) && (smallestDist<distA+margin) || (smallestDist < distA) && (smallestDist>distA-margin)) {
+                        img.setRGB(curX, curY, 0);
+                    }
+
+                }
+
+            }
+        }
+
         for (int i = 0; i < numberOfPoints; i++) {
             g.fill(new Ellipse2D .Double(Math.abs(posX[i] - 2.5), Math.abs(posY[i] - 2.5), 5, 5));
         }
+
+
+    }
+
+    static double euclidian(int x1, int x2, int y1, int y2) {
+        double d;
+        d = Math.sqrt((x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2)); // Euclidian
+        //  d = Math.abs(x1 - x2) + Math.abs(y1 - y2); // Manhattan
+        //  d = Math.pow(Math.pow(Math.abs(x1 - x2), p) + Math.pow(Math.abs(y1 - y2), p), (1 / p)); // Minkovski
+        return d;
     }
 
     public void paint(Graphics g) {
