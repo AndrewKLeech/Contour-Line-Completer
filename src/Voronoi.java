000ffbe8c5fb178b3dc3d2fa8375797150d20c31
AndrewKLeech <andrewkennethleech@gmail.com>
2017-11-15 16:42:44 +0000

    window with white background ready for points

--- src/Voronoi.java
+++ src/Voronoi.java
@@ -1,3 +1,8 @@
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.geom.Ellipse2D;
+import java.awt.image.BufferedImage;
 import java.util.*;
 import javax.swing.JFrame;
 
@@ -5,10 +10,24 @@ public class Voronoi extends JFrame{
     private List<Vector> voronoiPoints = new ArrayList<>();
     private static int height = 500;
     private static int width = 500;
+    static BufferedImage img;
+    //set number of test points
+    static int numberOfPoints = 50;
     private Voronoi (){
         super("Contour Connector");
         setBounds(0, 0, width, height);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
+        Random rand = new Random();
+
+        img = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
+        Graphics2D g = img.createGraphics();
+        g.setPaint ( new Color ( 255, 255, 255 ) );
+        g.fillRect ( 0, 0, width, height);
+        g.setColor(Color.BLACK);
+    }
+
+    public void paint(Graphics g) {
+        g.drawImage(img, 0, 0, this);
     }
     public static void main(String[] args){
         new Voronoi().setVisible(true);
