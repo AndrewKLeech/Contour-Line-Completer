c4a5e09fc732b39483e50133566ef9b2c3253112
AndrewKLeech <andrewkennethleech@gmail.com>
2017-11-15 16:57:25 +0000

    Make random points and display

--- src/Voronoi.java
+++ src/Voronoi.java
@@ -7,7 +7,7 @@ import java.util.*;
 import javax.swing.JFrame;
 
 public class Voronoi extends JFrame{
-    private List<Vector> voronoiPoints = new ArrayList<>();
+    private List<VPoint> vPoints = new ArrayList<>();
     private static int height = 500;
     private static int width = 500;
     static BufferedImage img;
@@ -18,12 +18,21 @@ public class Voronoi extends JFrame{
         setBounds(0, 0, width, height);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         Random rand = new Random();
+        for(int i = 0; i<numberOfPoints; i++){
+            VPoint vPoint = new VPoint(rand.nextInt(width),rand.nextInt(height));
+            vPoints.add(vPoint);
+            System.out.println("Point " + i + "\nX: " + vPoints.get(i).getX() + "\nY: " + vPoints.get(i).getY());
+        }
 
         img = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
         Graphics2D g = img.createGraphics();
         g.setPaint ( new Color ( 255, 255, 255 ) );
         g.fillRect ( 0, 0, width, height);
         g.setColor(Color.BLACK);
+
+        for (int i = 0; i < numberOfPoints; i++) {
+            g.fill(new Ellipse2D .Double(Math.abs(vPoints.get(i).getX() - 2.5), Math.abs(vPoints.get(i).getY() - 2.5), 5, 5));
+        }
     }
 
     public void paint(Graphics g) {
